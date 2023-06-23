0dbdaeb61e6190b52493fe0962bef455472e1235
Marcia Godoy <114769251+MarciaGodoy@users.noreply.github.com>
2023-06-23 16:01:26 -0300

    Add files via upload

---
+++ dicionario.py
@@ -0,0 +1,29 @@
+
+#Programa dicionario
+#Descricao: Este programa apresenta a estrutura de dados "dicionario"
+#em Python para fins de aprendizado.
+#Autor: Marcia
+#Data: 23/06/2023
+#Versao: 0.0.1
+
+
+# Atribuicao de variaveis
+
+tabela = { "Arroz": 0.5, "Batata": 0.2,   "Carne": 0.1 }
+
+# Entrada de dados
+
+
+
+#Processamento de dados
+
+
+
+# Saida de dados
+#print(tabela["Arroz"])
+
+
+
+ print(tabela{"Alface"})
+
+# print(tabela"Alface")
