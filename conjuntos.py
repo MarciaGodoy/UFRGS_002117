25b78582aee4632aa3430b48023afc131273781b
Marcia Godoy <114769251+MarciaGodoy@users.noreply.github.com>
2023-06-23 16:01:01 -0300

    Add files via upload

---
+++ conjuntos.py
@@ -0,0 +1,41 @@
+#Programa dicionario
+#Descricao: Este programa apresenta a estrutura de dados "dicionario"
+#em Python para fins de aprendizado.
+#Autor: Marcia
+#Data: 23/06/2023
+#Versao: 0.0.1
+
+
+# Atribuicao de variaveis
+
+lista = [1,2,3,6,9,3,5,2]
+conjunto_1 = set(lista)
+
+conjunto_2 = {1, 3, 3, 5, 5, 0, 23, -2}
+
+
+# Entrada de dados
+
+
+
+#Processamento de dados
+
+
+
+
+# Saida de dados
+print(conjunto_1)
+
+
+conjunto_1.remove(1)
+
+print(conjunto_2)
+
+conjunto_2.add(49
+            )
+
+print(conjunto_2)
+
+
+
+print(conjunto_1)
